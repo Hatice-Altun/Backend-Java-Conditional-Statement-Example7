5d6c677090006807268f5f161f9b6b1be93b98f1
Hatice-Altun <van.altun@outlook.com>
2023-10-21 17:10:26 +0200

    Switch Statement Example

---
+++ src/com/altun/Java/Main.java
@@ -0,0 +1,30 @@
+package com.altun.Java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// How to write a switch statement for the days of the week
+
+        int day= 1;
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+            case 2:
+                System.out.println("Tuesday");
+            case 3:
+                System.out.println("Wednesday");
+            case 4:
+                System.out.println("Thursday");
+            case 5:
+                System.out.println("Friday");
+            case 6:
+                System.out.println("Saturday");
+            case 7:
+                System.out.println("Sunday");
+            default:
+                System.out.println("Onbekende dag");
+                break;
+        }
+    }
+}
